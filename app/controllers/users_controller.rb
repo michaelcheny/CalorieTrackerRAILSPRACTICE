33f38cd50c055a6871c4df6369b0667e70530c35
Michael Chen <michaelchengaming@gmail.com>
2019-09-19 18:33:41 -0700

    add before action for finding user, do a bit of stuff in show user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :find_user
+
   def new
     @user = User.new
   end
@@ -9,7 +11,7 @@ class UsersController < ApplicationController
   end
 
   def show
-
+    
   end
 
   def edit
@@ -29,4 +31,9 @@ class UsersController < ApplicationController
   def user_params(*args)
     params.require(:user).permit(*args)
   end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
