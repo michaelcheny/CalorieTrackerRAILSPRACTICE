d5890d599b8630d5b3fe69dcb20b00289e2a5888
Michael Chen <michaelchengaming@gmail.com>
2019-09-19 18:13:33 -0700

    create restful controllers actions for user, add current user and logged in helpers for application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params(*args)
+    params.require(:user).permit(*args)
+  end
 end
